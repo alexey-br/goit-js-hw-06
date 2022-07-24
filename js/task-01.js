020cb656615d38e5334c8955f08aa8ab5a535122
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-24 11:37:46 +0300

    доработано задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,10 @@ const categoriesRef = document.querySelectorAll('li.item');
 
 console.log('Number of categories:', categoriesRef.length);
 
-for (const categorie of categoriesRef) {
+categoriesRef.forEach(categorie => {
   const categorieHeaderRef = categorie.querySelector('h2');
   console.log('Category:', categorieHeaderRef.textContent);
 
   const categorieItemsRef = categorie.querySelectorAll('li');
   console.log('Elements:', categorieItemsRef.length);
-}
+});
